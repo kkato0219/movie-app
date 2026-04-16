bab91644852775305c23fd552ee859e05df47055
ken-ichi kato <kkato0219@yahoo.com>
2026-04-17 02:31:55 +0900

    fix ReactDOM import

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
@@ -10,4 +10,4 @@ ReactDom.createRoot(document.getElementById("root")).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
